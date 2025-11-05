efa3b1cfbf79c8937e5cd9a0bdf13fe7ff9c2dc7
Zencaw <161458851+Zencaaw@users.noreply.github.com>
2025-11-05 16:33:43 +0100

    fix: add pages

--- frontend-web/src/components/Login.tsx
+++ frontend-web/src/components/Login.tsx
@@ -1,4 +1,4 @@
-import { use, useState } from "react";
+import { useState } from "react";
 
 function Login() {
     const [mail, setMail] = useState<string>("");
---
+++ frontend-web/src/pages/login.tsx
@@ -0,0 +1,11 @@
+import Login from "../components/Login";
+
+function login() {
+    return (
+        <>
+            <Login></Login>
+        </>
+    )
+}
+
+export default Login;

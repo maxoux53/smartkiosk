f4b3b65348e8a1dd1929c52ee89902a7a3d0ddfd
Zencaw <161458851+Zencaaw@users.noreply.github.com>
2025-11-06 23:13:59 +0100

    fix: useless component

--- frontend-web/src/pages/Login.tsx
+++ frontend-web/src/pages/Login.tsx
@@ -1,12 +1,37 @@
 import type { JSX } from "react";
-import LoginComponent from "../components/Login";
+import { useState } from "react";
 
 function Login(): JSX.Element {
-    return (
-        <>
-            <LoginComponent></LoginComponent>
-        </>
-    )
+    const [mail, setMail] = useState<string>("");
+        const [password, setPassword] = useState<string>("");
+        const [connectionMessage, setConnectionMessage] = useState<string>("");
+    
+        function login(): void {
+            console.log("test")
+            if (mail === "test" && password === "mdp"){
+                setConnectionMessage("Connexion réussie");
+            } else {
+                setConnectionMessage("Connexion échouée");
+            }
+        }
+    
+        return (
+            <main className="login">
+                <h1>Login</h1>
+                <form>
+                    <label>Email
+                        <input type="email" placeholder="Entrez une adresse email" onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setMail(e.target.value)}}/>
+                    </label>
+                    <label>Mot de passe
+                        <input type="password" placeholder="Entrez un mot de passe" onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setPassword(e.target.value)}}/>
+                    </label>
+                    
+                    <input type="submit" value="Se connecter"/>
+                </form>
+                
+                {connectionMessage}
+            </main>
+        )
 }
 
 export default Login;

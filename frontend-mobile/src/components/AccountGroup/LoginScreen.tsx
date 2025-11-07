0cda78f3e03a780ec34b3fbef5f172b6443af93f
Max <71935156+maxoux53@users.noreply.github.com>
2025-11-07 10:10:10 +0100

    refactor: reorder screens

--- frontend-mobile/src/components/AccountGroup/LoginScreen.tsx
+++ frontend-mobile/src/components/AccountGroup/LoginScreen.tsx
@@ -1,10 +1,16 @@
 import { JSX } from 'react';
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { AccountStackParamList } from '../../types/navigation';
 
 export default function LoginScreen() : JSX.Element {
+    const navigation : NativeStackNavigationProp<AccountStackParamList> = useNavigation();
+
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text>Connexion !</Text>
+            <Button title="Go to Profile" onPress={() => navigation.navigate('Profile')} />
         </View>
     );
 }

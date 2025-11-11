96c6372400e22d6c33affa2f318450190131fd15
Max <71935156+maxoux53@users.noreply.github.com>
2025-11-12 00:48:38 +0100

    feat: get, create & update product

--- backend/src/controller/product.ts
+++ backend/src/controller/product.ts
@@ -1,5 +1,6 @@
-import prisma from "../database/databaseORM.js";
+import prisma from "../database/databaseORM.ts";
 import { Request, Response } from "express";
+import { product } from "../generated/prisma/client.ts";
 
 export const getProduct = async (req : Request, res : Response) : Promise<void> => {
     try {
@@ -8,11 +9,82 @@ export const getProduct = async (req : Request, res : Response) : Promise<void> 
                 id: parseInt(req.params.id)
             }
         });
+
         if(product){
             res.send(product);
         } else {
             res.sendStatus(404);
         }
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
+};
+
+export const createProduct = async (req : Request, res : Response) : Promise<void> => {
+    try {
+        /* possibilité 1
+        const { label, is_available, excl_vat_price, deletion_date, picture, category_id, event_id } : product = req.body;
+
+        const newProductId : { id : number} = await prisma.product.create({
+            data: {
+                label,
+                is_available,
+                excl_vat_price,
+                deletion_date,
+                picture,
+                category_id,
+                event_id
+            },
+            select: {
+                id: true
+            }
+        }); */
+
+        // possibilité 2
+        const newProductId : { id : number} = await prisma.product.create({
+            data: {
+                label: req.body.label,
+                is_available: req.body.is_available,
+                excl_vat_price: req.body.excl_vat_price,
+                deletion_date: req.body.deletion_date,
+                picture: req.body.picture,
+                category_id: req.body.category_id,
+                event_id: req.body.event_id
+            },
+            select: {
+                id: true
+            }
+        });
+        
+        res.status(201).send(newProductId);
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
+};
+
+export const updateProduct = async (req : Request, res : Response) : Promise<void> => {
+    try {
+        const { id, label, is_available, excl_vat_price, deletion_date, picture, category_id, event_id } : product = req.body;
+        // const safeExclVatPriceDecimal = new prisma.Decimal(excl_vat_price);
+
+        const updatedProduct = await prisma.product.update({
+            where: {
+                id
+            },
+            data: {
+                label,
+                is_available,
+                excl_vat_price,
+                deletion_date,
+                picture,
+                category_id,
+                event_id
+            }
+        });
+
+        res.send(updatedProduct);
     } catch (err) {
         console.error(err);
         res.sendStatus(500);

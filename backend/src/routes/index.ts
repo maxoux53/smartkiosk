5fd322bae18bd9088c2df663e54f8a2fcdb903ac
Max <71935156+maxoux53@users.noreply.github.com>
2025-11-12 15:12:45 +0100

    feat: add route templates

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,21 @@
 import { Router, type Request, type Response } from "express";
 import { default as productRouter } from "./product.ts";
+import { default as categoryRouter } from "./category.ts";
+import { default as eventRouter } from "./event.ts";
+import { default as imageRouter } from "./image.ts";
+import { default as purchaseRouter } from "./purchase.ts";
+import { default as userRouter } from "./user.ts";
+import { default as vatRouter } from "./vat.ts";
 
 const router = Router();
 
 router.use("/product", productRouter);
+router.use("/category", categoryRouter);
+router.use("/event", eventRouter);
+router.use("/image", imageRouter);
+router.use("/purchase", purchaseRouter);
+router.use("/user", userRouter);
+router.use("/vat", vatRouter);
 
 // Gestion d'une URL hors application
 router.use((req : Request, res : Response) : Response => {

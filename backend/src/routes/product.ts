7e6fc4e72b217dab86992e2ade349849395d7794
Max <71935156+maxoux53@users.noreply.github.com>
2025-11-17 00:06:54 +0100

    docs: swagger experiment

--- backend/src/routes/product.ts
+++ backend/src/routes/product.ts
@@ -11,6 +11,44 @@ import { productVal } from '../middleware/validation/validator.ts';
 
 const router = Router();
 
+/**
+ * @swagger
+ * /products/{id}:
+ *   get:
+ *     summary: Get a product by ID
+ *     tags:
+ *       - Product
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: Numeric ID of the product to get
+ *     responses:
+ *       200:
+ *         description: Product found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       400:
+ *         description: Invalid ID supplied
+ *         content:
+ *           text/plain:
+ *             schema:
+ *               type: string
+ *       404:
+ *         description: Product not found
+ *         content:
+ *           text/plain:
+ *             schema:
+ *               type: string
+ *       500:
+ *         description: Server error
+ */
 router.get('/:id', productVal.get, getProduct);
 router.post('/', productVal.create, createProduct);
 router.patch('/', productVal.update, updateProduct);

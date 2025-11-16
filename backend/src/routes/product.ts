8e21568669c66e316451c5c0278e268474f03920
Max <71935156+maxoux53@users.noreply.github.com>
2025-11-16 19:02:22 +0100

    feat: implement product validators in route

--- backend/src/routes/product.ts
+++ backend/src/routes/product.ts
@@ -1,15 +1,19 @@
 import { Router } from 'express';
+
 import {
     getProduct,
     createProduct,
     updateProduct,
     deleteProduct
 } from "../controller/product.ts";
+
+import { productVal } from '../middleware/validation/validator.ts';
+
 const router = Router();
 
-router.get('/:id', getProduct);
-router.post('/', createProduct);
-router.patch('/', updateProduct);
-router.delete('/:id', deleteProduct);
+router.get('/:id', productVal.get, getProduct);
+router.post('/', productVal.create, createProduct);
+router.patch('/', productVal.update, updateProduct);
+router.delete('/:id', productVal.delete, deleteProduct);
 
 export default router;

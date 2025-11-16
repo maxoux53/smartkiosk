e828d8c06b79d52f8813dab20b7aa082f48c2764
Max <71935156+maxoux53@users.noreply.github.com>
2025-11-16 18:48:31 +0100

    feat: add product validator

---
+++ backend/src/middleware/validation/product.ts
@@ -0,0 +1,30 @@
+import vine from '@vinejs/vine'
+
+const productIdSchema = vine.object({
+    id: vine.number()
+});
+
+const productCreationSchema = vine.object({
+    label: vine.string().minLength(1).maxLength(80),
+    is_available: vine.boolean().optional(),
+    excl_vat_price: vine.number().min(0),
+    deletion_date: vine.date().optional(),
+    category_id: vine.number(),
+    event_id: vine.number()
+});
+
+const productUpdateSchema = vine.object({
+    id: vine.number(),
+    label: vine.string().minLength(1).maxLength(80).optional(),
+    is_available: vine.boolean().optional(),
+    excl_vat_price: vine.number().min(0).optional(),
+    deletion_date: vine.date().optional(),
+    category_id: vine.number().optional(),
+    event_id: vine.number().optional()
+});
+
+export const
+    productCreation = vine.compile(productCreationSchema),
+    productUpdate = vine.compile(productUpdateSchema),
+    productDeletion = vine.compile(productIdSchema)
+;
---
+++ backend/src/middleware/validation/validator.ts
@@ -0,0 +1,32 @@
+import { ValidationError } from '@vinejs/vine';
+import * as productSchemas from './product.ts';
+import { Request, Response, NextFunction } from 'express';
+
+export const productVal = {
+    create: async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            req.body = await productSchemas.productCreation.validate(req.body);
+            next();
+        } catch (e) {
+            res.status(400).send((e as ValidationError).message);
+        }
+    },
+    update: async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            req.body = await productSchemas.productUpdate.validate(req.body);
+            next();
+        } catch (e) {
+            res.status(400).send((e as ValidationError).message);
+        }
+    },
+    delete: async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            req.body = await productSchemas.productDeletion.validate(req.body);
+            next();
+        } catch (e) {
+            res.status(400).send((e as ValidationError).message);
+        }
+    }
+};
+
+// il faudrait essayer d'éliminer l'énorme duplication de code

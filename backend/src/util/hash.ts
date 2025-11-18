495d5d152fac601df2aa162c72e8042879457698
Max <71935156+maxoux53@users.noreply.github.com>
2025-11-18 17:06:36 +0100

    refactor: store pepper in a var + clearer plain text

--- backend/src/util/hash.ts
+++ backend/src/util/hash.ts
@@ -1,13 +1,15 @@
 import * as argon2 from 'argon2';
 
+const binaryPepper = Buffer.from(process.env.PEPPER!);
+
 export function hash(password: string) : Promise<string> {
-    return argon2.hash(password, {secret: Buffer.from(process.env.PEPPER)});
+    return argon2.hash(password, {secret: binaryPepper});
 };
 
-export function compare(plainText: string, hash: string) : Promise<boolean> {
+export function compare(plainTextPassword: string, hash: string) : Promise<boolean> {
     return argon2.verify(
         hash,
-        plainText,
-        {secret: Buffer.from(process.env.PEPPER)}
+        plainTextPassword,
+        {secret: binaryPepper}
     );
 };

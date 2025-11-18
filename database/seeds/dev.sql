dd1407b6f1a8683e4abaffbb2f5d6588a02bd39f
Max <71935156+maxoux53@users.noreply.github.com>
2025-11-18 18:31:55 +0100

    tests: update dev seed to use actual stored data

--- database/seeds/dev.sql
+++ database/seeds/dev.sql
@@ -5,18 +5,18 @@ BEGIN TRANSACTION;
   ('C', 6);
 
   INSERT INTO category (label, vat_type, picture) VALUES
-  ('Nourriture', 'A', 'https://www.morelandobgyn.com/hubfs/Imported_Blog_Media/GettyImages-854725402-1.jpg'),
-  ('Boissons', 'B', 'https://www.shutterstock.com/image-photo/summer-red-alcoholic-cocktail-drink-600nw-2496149251.jpg'),
-  ('Marchandises', 'C', 'https://store.wikimedia.org/cdn/shop/files/Untitled_Artwork_281_16.jpg');
+  ('Nourriture', 'A', 'e8801ca5-b73f-4226-8682-56f98a88fb00'),
+  ('Boissons', 'B', '4abb63a9-959f-4e03-9e47-4ecfebb9a000'),
+  ('Marchandises', 'C', '5d401cb3-aad8-4d91-2665-49d1fd737600');
 
-  INSERT INTO "user" (first_name, last_name, email, password, is_admin, avatar) VALUES
-  ('Jean', 'Dupont', 'jean.dupont@example.com', '\\x1234567890abcdef', TRUE, 'https://avatar.iran.liara.run/public/34.jpeg'),
-  ('Marie', 'Martin', 'marie.martin@example.com', '\\xabcdef1234567890', FALSE, 'https://avatar.iran.liara.run/public/65.jpeg'),
-  ('Pierre', 'Dubois', 'pierre.dubois@example.com', '\\x9876543210fedcba', FALSE, 'https://avatar.iran.liara.run/public/44.jpeg');
+  INSERT INTO "user" (first_name, last_name, email, password_hash, is_admin, avatar) VALUES
+  ('Jean', 'Dupont', 'jean.dupont@example.com', '$argon2id$v=19$m=65536,t=3,p=4$V000lTnXaHR9MC92KlXw9g$gkW3/en6nzYyD8B8xBwLDJzLUsPMQvN2Qvdl5ZYlSLo', TRUE, '9f0855fa-ce19-4658-292a-34d5972ba100'),
+  ('Marie', 'Martin', 'marie.martin@example.com', '$argon2id$v=19$m=65536,t=3,p=4$WP/x1wgMRo+KrjFhulmPyA$7c28Ij7ABG6BN27Km0S0oyePYFmigvY4Yfp6rKR34wo', FALSE, '9c7f5c46-9914-477f-7e58-83a82988a900'),
+  ('Pierre', 'Dubois', 'pierre.dubois@example.com', '$argon2id$v=19$m=65536,t=3,p=4$0dp8qdIlpBU/weXBh56iXw$MeOTNR7dC7+n6zSA/cNtdYEd8lTa7cba1DNzg8yWWfQ', FALSE, '2edf7a61-0e6e-4988-6b8a-613679f19f00');
 
   INSERT INTO event (name, location, image, iban) VALUES
-  ('Festival d''Été', 'Parc Central', 'https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i82f8384a1348ab84/version/1554202007/tomorrowland-best-summer-music-festivals-europe.jpg', 'GB29 NWBK 6016 1331 9268 19'),
-  ('Conférence Tech', 'Centre des Congrès', 'https://quixy.com/wp-content/uploads/2021/04/IDC-Directions.jpg', 'DE89 3704 0044 0532 0130 00');
+  ('Festival d''Été', 'Parc Central', '021d1aea-2420-41d1-f230-f6fd975fc100', 'GB29 NWBK 6016 1331 9268 19'),
+  ('Conférence Tech', 'Centre des Congrès', '9e0c2140-ab35-4942-e3e4-445402632300', 'DE89 3704 0044 0532 0130 00');
 
   INSERT INTO membership (user_id, event_id, role) VALUES
   (1, 1, 'host'),
@@ -24,9 +24,9 @@ BEGIN TRANSACTION;
   (3, 2, 'guest');
 
   INSERT INTO product (label, is_available, excl_vat_price, picture, category_id, event_id) VALUES
-  ('Burger', TRUE, 5.00, 'https://assets.orderbilly.com/products/502936_o.jpg', 1, 1),
-  ('Soda', TRUE, 2.50, 'https://euc1.posios.com/posimages/pos20@innovativefoodconcept.com_75458/images/products/08162984d3464070bbd1bef589fac0b6.png', 2, 1),
-  ('T-Shirt', TRUE, 15.00, 'https://euc1.posios.com/posimages/pos20@innovativefoodconcept.com_75458/images/products/d25fed6ef8da46d7810aa14a5803774d.png', 3, 2);
+  ('Burger', TRUE, 5.00, 'ce844a95-1ad2-465e-c554-fc9f74c42900', 1, 1),
+  ('Soda', TRUE, 2.50, '54a3f697-3417-46e3-d3fd-dab356f91f00', 2, 1),
+  ('T-Shirt', TRUE, 15.00, 'eaf413e3-8c02-4a2f-ea11-0f4082c44800', 3, 2);
 
   INSERT INTO purchase ("date", user_id) VALUES
   ('2023-10-01 10:00:00', 2),

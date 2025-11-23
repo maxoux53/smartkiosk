ec3102c6777df7dabdea94714d160553143b3c1c
Zencaw <161458851+Zencaaw@users.noreply.github.com>
2025-11-24 00:00:22 +0100

    feat: add admin main page

--- frontend-web/src/constant.ts
+++ frontend-web/src/constant.ts
@@ -1,4 +1,6 @@
-export const ADMIN_TABLES: Record<string, string> = {
+// Confirmer l'utilisation
+
+export const TABLES: Record<string, string> = {
     USERS: "Utilisateurs",
     MEMBERSHIPS: "Participations",
     EVENTS: "Évènements",
@@ -9,11 +11,13 @@ export const ADMIN_TABLES: Record<string, string> = {
     VATS: "TVA"
 };
 
-export const MANAGER_TABLES: Record<string, string> = {
-    EVENT: "Évènement", // Only his event
-    USERS: "Utilisateurs", // Read only
-    MEMBERSHIPS: "Participations",
-    PURCHASES: "Achats",
-    ORDER_LINES: "Lignes de commandes",
-    PRODUCTS: "Produits",
-}
+export const USERS_ATTRIBUTES: Record<string, string> = {
+    id: "ID",
+    first_name: "Prénom",
+    last_name: "Nom",
+    email: "Email",
+    password: "Mot de passe",
+    avatar: "Avatar",
+    is_admin: "Est admin",
+    deletion_date: "Date de suppression"
+};
---
+++ frontend-web/src/pages/admin/Admin.tsx
@@ -0,0 +1,53 @@
+import { useState, type JSX } from "react";
+import { TABLES } from "../../constant";
+import UserTable from "../../components/admin/table/User";
+import MembershipTable from "../../components/admin/table/Membership";
+import EventTable from "../../components/admin/table/Event";
+import PurchaseTable from "../../components/admin/table/Purchase";
+import OrderLineTable from "../../components/admin/table/OrderLine";
+import ProductTable from "../../components/admin/table/Product";
+import CategoryTable from "../../components/admin/table/Category";
+import VatTable from "../../components/admin/table/Vat";
+
+export default function Admin(): JSX.Element {
+    const [element, setElement] = useState<JSX.Element>(<></>);
+
+    return (
+        <main>
+            <h1>Administrateur</h1>
+
+            <div role="group">
+                <button
+                    onClick={(): void => {
+                        setElement(<UserTable />);
+                    }}
+                >
+                    {TABLES.USERS}
+                </button>
+                <button onClick={(): void => setElement(<MembershipTable />)}>
+                    {TABLES.MEMBERSHIPS}
+                </button>
+                <button onClick={(): void => setElement(<EventTable />)}>
+                    {TABLES.EVENTS}
+                </button>
+                <button onClick={(): void => setElement(<PurchaseTable />)}>
+                    {TABLES.PURCHASES}
+                </button>
+                <button onClick={(): void => setElement(<OrderLineTable />)}>
+                    {TABLES.ORDER_LINES}
+                </button>
+                <button onClick={(): void => setElement(<ProductTable />)}>
+                    {TABLES.PRODUCTS}
+                </button>
+                <button onClick={(): void => setElement(<CategoryTable />)}>
+                    {TABLES.CATEGORIES}
+                </button>
+                <button onClick={(): void => setElement(<VatTable />)}>
+                    {TABLES.VATS}
+                </button>
+            </div>
+
+            {element}
+        </main>
+    );
+}

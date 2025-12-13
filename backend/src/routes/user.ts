408750c791d7b1c7d8a3c069b4043152d3fc5e91
Max <71935156+maxoux53@users.noreply.github.com>
2025-12-13 16:08:09 +0100

    feat: add user route

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,19 +1,18 @@
 import { Router } from 'express';
-import { replaceUserAvatar } from '../middleware/image-replacement.ts';
 import {
-    getProduct,
-    getAllProducts,
-    createProduct,
-    updateProduct,
-    deleteProduct
-} from "../controller/product.ts";
+    getUser,
+    getAllUsers,
+    createUser,
+    updateUser,
+    deleteUser
+} from "../controller/user.ts";
 
 const router = Router();
 
-router.get('/:id', getProduct);
-router.get('/', getAllProducts);
-router.post('/', createProduct);
-router.patch('/', replaceUserAvatar, updateProduct);
-router.delete('/:id', deleteProduct);
+router.get('/:id', getUser);
+router.get('/', getAllUsers);
+router.post('/', createUser);
+router.patch('/', updateUser);
+router.delete('/:id', deleteUser);
 
 export default router;

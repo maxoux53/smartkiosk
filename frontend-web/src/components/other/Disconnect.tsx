d37f5af9a5f59be4c6513efc0dc48917aea54e7e
Zencaw <161458851+Zencaaw@users.noreply.github.com>
2025-12-18 20:42:41 +0100

    fix : lint

--- frontend-web/src/components/other/Disconnect.tsx
+++ frontend-web/src/components/other/Disconnect.tsx
@@ -2,12 +2,12 @@ import { useState, type JSX } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Disconnect(): JSX.Element {
-    const [showDisconnectModal, setShowDisconnectModal] = useState<Boolean>(false);
+    const [showDisconnectModal, setShowDisconnectModal] = useState<boolean>(false);
     const navigate = useNavigate();
 
     return (
         <>
-            <button onClick={() => setShowDisconnectModal(true)}>Se déconnecter</button>
+            <button type="button" onClick={() => setShowDisconnectModal(true)}>Se déconnecter</button>
             {
                 showDisconnectModal ? (
                     <dialog open>
@@ -19,8 +19,8 @@ export default function Disconnect(): JSX.Element {
                                 Voulez-vous vraiment vous déconnecter ?
                             </p>
                             <footer>
-                                <button onClick={() => navigate("/login")}>Oui</button>
-                                <button className="secondary" onClick={() => setShowDisconnectModal(false)}>Non</button>
+                                <button type="button" onClick={() => navigate("/login")}>Oui</button>
+                                <button type="button" className="secondary" onClick={() => setShowDisconnectModal(false)}>Non</button>
                             </footer>
                         </article>
                     </dialog>

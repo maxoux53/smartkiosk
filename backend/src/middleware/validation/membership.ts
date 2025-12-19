d0467821e7c9ce73889409ece7391d788ecd3c9f
ElleryRR <Cdt.martin@icloud.com>
2025-12-19 14:49:12 +0100

    delete: update membership vine schema

--- backend/src/middleware/validation/membership.ts
+++ backend/src/middleware/validation/membership.ts
@@ -8,6 +8,7 @@ const user_email = vine.string().email();
 const role = vine.enum(Object.values(membership_role)).optional();
 
 const membershipIdSchema = vine.object({
+    user_id,
     event_id
 });
 
@@ -20,12 +21,6 @@ const eventJoinSchema = vine.object({
     event_id
 });
 
-const membershipUpdateSchema = vine.object({
-    user_id,
-    event_id,
-    role
-});
-
 const cashiersByEventSchema = vine.object({
     event_id
 });
@@ -34,7 +29,6 @@ export const
     membershipSearch = vine.create(membershipIdSchema),
     membershipCreation = vine.create(membershipCreationSchema),
     eventJoin = vine.create(eventJoinSchema),
-    membershipUpdate = vine.create(membershipUpdateSchema),
     membershipDeletion = vine.create(membershipIdSchema),
     cashiersByEvent = vine.create(cashiersByEventSchema)
 ;
